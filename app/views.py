e0d145192cbf412ca9647b6cd1b3d7b951fc865f
kleshklya <tyzik20@ya.ru>
2014-05-20 00:05:19 +0400

    template/index/bootstrat666

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from lists.utils import render_and_respond
 from app.models import Sheet
+import logging
+logger = logging.getLogger('app-views')
 
 # Create your views here.
 
 def index(request):
     sheet = Sheet.objects.all()
+    logger.info(sheet)
     return render_and_respond(request, 'app/index.html', {'sheet':sheet})
+
+def addsheet(request):
+    return render_and_respond(request, 'app/addsheet.html', {})
     
--- lists/urls.py
+++ lists/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', include('app.urls')),
+    url(r'^', include('app.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
