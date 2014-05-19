5db91d6c09ae580f295a63efca41b127b7674f21
kleshklya <tyzik20@ya.ru>
2014-05-19 20:49:14 +0400

    template/index/bootstrat

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from lists.utils import render_and_respond
+from app.models import Sheet
 
 # Create your views here.
 
 def index(request):
-    return render_and_respond(request, 'app/index.html', {})
+    sheet = Sheet.objects.all()
+    return render_and_respond(request, 'app/index.html', {'sheet':sheet})
+    
